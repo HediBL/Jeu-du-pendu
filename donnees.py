de93f0d7f265e112eff775f2d52d4cbd15805d03
HediBL <69821046+HediBL@users.noreply.github.com>
2020-11-17 18:34:35 +0100

    Create donnees.py

---
+++ donnees.py
@@ -0,0 +1,13 @@
+#Ce fichier contient les variables nécessaires au fonctionnement
+#de l'appli (liste de mots, nombre de chances autorisées, le score...)
+
+nb_coups = 8
+
+nom_fichier_scores = "scores"
+
+liste_mots = [ abêtiras, conjurez, razziées, ardentes, détersif, zestâtes, créditez, empestât, 
+chienner, navrante, éclusier, loveriez, assiérez, préposer, escroqua, trouviez, mercerie, 
+réglages, sauvâtes, écrémiez, mordisse, repolies, profanas, fonderez, vouerais, phénique, 
+rempares, houillas, appontai, ingérant, calambac, stéatose, théistes, fourbira, limonait, 
+boudinas, itérâmes, rénovait, proposer, saureras, polentas, saoudite, azurâmes, enfichée, 
+divulgua, vaillent, berneras, gageasse, dénigrai, écourtez]
